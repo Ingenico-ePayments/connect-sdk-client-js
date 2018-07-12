a720437deeaa6b83206515d5030cccdd1b1d07fd
Ingenico ePayments <jenkins@isaac.nl>
2018-07-12 10:00:15 +0200

    Release 3.9.1.

--- dist/connectsdk.js
+++ dist/connectsdk.js
@@ -13689,7 +13689,7 @@ define("connectsdk.Util", ["connectsdk.core"], function (connectsdk) {
 					return {
 						screenSize: window.innerWidth + "x" + window.innerHeight,
 						platformIdentifier: window.navigator.userAgent,
-						sdkIdentifier: ((document.GC && document.GC.rppEnabledPage) ? 'rpp-' : '') + 'JavaScriptClientSDK/v3.9.0',
+						sdkIdentifier: ((document.GC && document.GC.rppEnabledPage) ? 'rpp-' : '') + 'JavaScriptClientSDK/v3.9.1',
 						sdkCreator: 'Ingenico'
 					};
 				},
--- src/C2SPaymentProductContext.js
+++ src/C2SPaymentProductContext.js
@@ -1,12 +1,14 @@
 define("connectsdk.C2SPaymentProductContext", ["connectsdk.core"], function(connectsdk) {
 
-  var C2SPaymentProductContext = function (payload) {
-	this.totalAmount = payload.totalAmount;
-	this.countryCode = payload.countryCode;
-	this.isRecurring = payload.isRecurring;
-	this.currency = payload.currency;
-	this.locale = payload.locale;
-  };
+    var C2SPaymentProductContext = function (payload) {
+        this.totalAmount = (payload.totalAmount === undefined) ? '' : payload.totalAmount;
+        this.countryCode = payload.countryCode;
+        this.isRecurring = (payload.isRecurring === undefined) ? '' : payload.isRecurring;
+        this.currency = payload.currency;
+        if (payload.locale !== undefined){
+            this.locale = payload.locale
+        }
+    };
 
   connectsdk.C2SPaymentProductContext = C2SPaymentProductContext;
   return C2SPaymentProductContext;
